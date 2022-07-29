d1b0cfec6aed0328293a43ae06c4d61a14f7b208
Jeremiah King <usaxray25@gmail.com>
2022-07-29 06:25:12 -0500

    select changing and updating firebase, no trendering on card or when console logged

--- pages/teams.js
+++ pages/teams.js
@@ -6,7 +6,6 @@ import { Button } from 'react-bootstrap';
 import { getTeams } from '../api/teamData';
 import { useAuth } from '../utils/context/authContext';
 import TeamCard from '../components/TeamCard';
-// import SearchTeams from '../components/SearchTeams';
 
 export default function Teams() {
   const [teams, setTeams] = useState([]);
@@ -15,10 +14,6 @@ export default function Teams() {
 
   const getAllTeams = () => {
     getTeams(user.uid).then(setTeams);
-    // console.warn(getTeams);
-    // setTeams(teamsArray);
-  //     setFilteredTeams(teamsArray);
-  //   });
   };
 
   useEffect(() => {
@@ -29,16 +24,13 @@ export default function Teams() {
     <>
       <h1>Team</h1>
       <div className="text-center my-4">
-        {/* <SearchTeams teams={teams} setFilteredTeams={setFilteredTeams} /> */}
-        <div className="text-center my-4">
-          <Link href="/team/new" passHref>
-            <Button variant="warning">Add A Team</Button>
-          </Link>
-          <div className="d-flex flex-wrap">
-            {teams.map((team) => (
-              <TeamCard key={team.firebaseKey} teamObj={team} src={team.logoUrl} user={user} onUpdate={getAllTeams} />
-            ))}
-          </div>
+        <Link href="/team/new" passHref>
+          <Button variant="warning">Add A Team</Button>
+        </Link>
+        <div className="d-flex flex-wrap">
+          {teams.map((team) => (
+            <TeamCard key={team.firebaseKey} teamObj={team} src={team.logoUrl} user={user} onUpdate={getAllTeams} />
+          ))}
         </div>
       </div>
     </>
