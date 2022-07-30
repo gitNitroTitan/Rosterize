6b7e0142b4ffd679f6d32864698a6eacd1ab1674
Jeremiah King <usaxray25@gmail.com>
2022-07-29 20:46:58 -0500

    fixed select to equal matching fbKey and called name of select in card

--- pages/player/[firebaseKey].js
+++ pages/player/[firebaseKey].js
@@ -4,10 +4,16 @@ import Card from 'react-bootstrap/Card';
 import Link from 'next/link';
 import Button from 'react-bootstrap/Button';
 import { viewPlayerDetails } from '../../api/mergedData';
+import { deletePlayer } from '../../api/playerData';
 
 function ViewPlayer() {
   const [playerDetails, setPlayerDetails] = useState({});
   const router = useRouter();
+  const deleteIndPlayer = (playerObjects, onUpdate) => {
+    if (window.confirm(`Delete ${playerObjects.name}?`)) {
+      deletePlayer(playerObjects.firebaseKey).then(() => onUpdate());
+    }
+  };
 
   const { firebaseKey } = router.query;
 
@@ -20,11 +26,14 @@ function ViewPlayer() {
       <Card.Img variant="top" src={playerDetails.imageUrl} alt={playerDetails.name} style={{ height: '400px' }} />
       <Card.Body>
         <Card.Title>{playerDetails.name}</Card.Title>
-        <p className="card-subtitle mb-2 text-muted">{playerDetails.position}</p>
+        <p className="card-subtitle mb-2 text-muted">{playerDetails.position}<br />{playerDetails.playersTeam}</p>
         <p className="card-subtitle mb-2 text-muted">{playerDetails.teamObject?.name}</p>
         <Link href={`/player/edit/${playerDetails.firebaseKey}`} passHref>
           <Button className="edit-btn" variant="info">EDIT</Button>
         </Link>
+        <Button variant="danger" onClick={deleteIndPlayer} className="m-2">
+          DELETE
+        </Button>
       </Card.Body>
     </Card>
   );
